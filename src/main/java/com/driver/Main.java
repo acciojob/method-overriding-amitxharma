3316015da51fcea7b442561b3300e8aff201b216
amit sharma <amitsharma75700@gmail.com>
2023-03-20 19:27:49 +0530

    in one file

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,12 +1,21 @@
 package com.driver;
 
-import org.w3c.dom.ls.LSOutput;
+class A {
+    public String meth() {
+        return "Invoking method from class A";
+    }
+}
+
+class B extends A {
+    @Override
+    public String meth() {
+        return "Method is overridden in Extended class B";
+    }
+}
 
 public class Main {
-    public static void main(String[] args)
-    {
-        B obj =new B();
-        String result1 = obj.meth();
-        String result2 = ((A)obj).meth();
+    public static void main(String[] args) {
+        B obj = new B();
+        System.out.println(obj.meth());
     }
 }
