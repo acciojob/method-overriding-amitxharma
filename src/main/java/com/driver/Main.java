e518ce6da3f450ed6d14d2343dda27f463436d73
amit sharma <amitsharma75700@gmail.com>
2023-03-20 18:36:35 +0530

    Overide Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
-  
+    public static void main(String[] args)
+    {
+        B obj =new B();
+        String ans= obj.meth();
+        System.out.println(ans);
+    }
 }
